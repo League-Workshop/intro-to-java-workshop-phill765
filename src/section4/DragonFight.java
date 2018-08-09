c02275c1d747382db0ea3f9bbef03a0f8c992a2e
League <league@WTS8.attlocal.net>
2018-08-09 12:01:44 -0700

    Day 4

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -2,6 +2,9 @@ package section4;
 
 import java.util.Random;
 import javax.swing.JOptionPane;
+
+import sun.net.www.content.image.jpeg;
+
 import javax.swing.ImageIcon;
 
 public class DragonFight {
@@ -12,43 +15,69 @@ public class DragonFight {
 		
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int playerDamage = 0;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
-		
+		int dragonDamage = 0;
 		
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
 		
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String AT = JOptionPane.showInputDialog("Do you want to attack that dragon with yell or kick?");
 		// 9. If they typed in "yell":
-		
+		if(AT.equals("yell")) {
+			int yellD = new Random().nextInt(11);
+			dragonDamage = yellD;
+			JOptionPane.showMessageDialog(null, "you yelled for " + yellD + " damage!");
+			dragonHealth -= yellD;
+		}
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
 			
 			//-- Subtract that number from the dragon's health variable 
 			
 		// 10. If they typed in "kick":
-		
+		if(AT.equals("kick")) {
+			int kickD = new Random().nextInt(26);
+			dragonDamage = kickD;
+			JOptionPane.showMessageDialog(null, "you kicked for " + kickD + " damage!" );
+			dragonHealth -= kickD;
+		}
+		if(AT.equals("BEGONE THOT")) {
+			dragonHealth = -500;
+		}
+		if(AT.equals("I have ligma")) {
+			playerHealth = -500;
+		}
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+		
 			//-- Subtract that number from the dragon's health variable
 			
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		int fireD = new Random().nextInt(36);
+		playerDamage = fireD;
 		// 12. Subtract this number from the player's health
-		
+		playerHealth -= playerDamage;
+		JOptionPane.showMessageDialog(null, "The dragon hit you with a stricking blow of " + playerDamage + " damage!");
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerHealth<=0) {
+			JOptionPane.showMessageDialog(null, "You were slain by the dragon! \n Better luck next time!");
+			JOptionPane.showMessageDialog(null, "The dragon had " + dragonHealth + " health left");
+		} else {
+			if(dragonHealth<=0) {
+				JOptionPane.showMessageDialog(null, "You killed the dragon!!!");
+				JOptionPane.showMessageDialog(null, "You had " + playerHealth + " health left");
+			}
+		}
 			//-- Tell the user that they lost
 			
 		
@@ -63,3 +92,4 @@ public class DragonFight {
 		}
 	}
 
+}
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,14 +1,38 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String easy = JOptionPane.showInputDialog("What is 2+2?");
 		// 3.  Use an if statement to check if their answer is correct
+		if(easy.equals("4")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score+=1;
+		} else {
+			JOptionPane.showMessageDialog(null, "The answer is actually 4. That wasn't that hard");
+		}
+		String harder = JOptionPane.showInputDialog("Who runs the company Microsoft?");
+		if(harder.equals("Bill Gates")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score+=1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Come on man!");
+		}
+		JOptionPane.showMessageDialog(null, "Time for the hardest question");
+		String RNB = JOptionPane.showInputDialog("What is the airspeed velocity of an unladen swallow?");
+		if(RNB.equals("What do you mean, an African or European Swallow?")) {
+			JOptionPane.showMessageDialog(null, "what? AHHHHHH.......");
+			score+=1;
+		} else {
+			JOptionPane.showMessageDialog(null, "Wrong!");
+		}
+		JOptionPane.showMessageDialog(null, "You scored " + score );
 		
 		// 4.  if the user's answer is correct
 		
